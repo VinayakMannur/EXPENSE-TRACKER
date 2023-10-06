60c696191c764e203b1c6a0b33a190993c856a4d
VinayakMannur <Vinayakmannur20@gmail.com>
2023-10-06 20:56:21 +0530

    Added all features except report

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,4 +1,6 @@
 import React,{useState, useEffect} from 'react'
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar} from 'antd';
 import {Outlet, useNavigate } from "react-router-dom";
 
 // import logo from '../png/logo1.png'
@@ -26,10 +28,11 @@ const Navbar = () => {
         <>
         <nav className="navbar bg-dark navbar-expand-lg px-5 py-0 bg-body-tertiary" data-bs-theme="dark">
             <div className="container-fluid my-2">
-                <a className="navbar-brand" href="/">
-                    <img  alt="Logo" width="30" height="24" className="d-inline-block align-text-top" /> Expense-Tracker
+                <a className="navbar-brand ml-5" href="/">
+                    {/* <img  alt="Logo" width="30" height="24" className="d-inline-block align-text-top" /> */}
+                     Expense-Tracker
                 </a>
-                <div className="collapse navbar-collapse d-flex" id="navbarNav">
+                <div className="collapse navbar-collapse d-flex ml-5" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="/">Home</a>
@@ -37,7 +40,8 @@ const Navbar = () => {
                     </ul>
                 </div>
                     <li className='me-2'>
-                        <span className="text-white text-uppercase font-weight-bold">{loginUser && loginUser.name}</span>
+                        <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} /> 
+                        <span className="text-white px-2 text-uppercase font-weight-bold">{loginUser && loginUser.name}</span>
                     </li>
                     <li className='me-1'>
                         <button className='btn btn-sm btn-success btn-block' onClick={logoutHandler}>Logout</button>
